ffb0c3ba8b6d36c3db29991ee2c06e8e094d7969
wakanda28 <begona.calderon@st-georgeit.com>
2022-01-12 19:02:14 +0100

    httyg

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ options = webdriver.ChromeOptions()
 #options.add_argument("--headless")
 
 driver = webdriver.Chrome(chrome_options=options)
-driver.get('https://www.e-pacallianz.com/ngx-epac-professional/public/home')
-
+#driver.get('https://www.e-pacallianz.com/ngx-epac-professional/public/home')
+driver.get("https://www.e-pacallianz.com/ngx-epac-professional/private/home#:~:text=Aplic.-,Allianz,-Encargos%20Proveedores%20Allianz")
 
 USR_element = driver.find_element_by_name("username")
 USR_element.send_keys(USERNAME)
@@ -21,8 +21,9 @@ PSWD_element.send_keys(PSWD)
 LGIN_element = driver.find_element_by_xpath("/html/body/div/div/app-root/app-public/app-public-home/div/div/div/app-login/div/form/div[2]/div/button")
 LGIN_element.click()
 
-APL_ALLIANZ_element = driver.find_element_by_name('Aplic. Allianz')
-APL_ALLIANZ_element.click()
+
+#APL_ALLIANZ_element = driver.fi("/html/body/div/div/app-root/app-private/app-private-header/div/nx-extended-navigation/div/div[1]/nx-extended-navigation-desktop/div[1]/div/div/ul/li[2]/a/span")
+#APL_ALLIANZ_element.click()
 
 
 
