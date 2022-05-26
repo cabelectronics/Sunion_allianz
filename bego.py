eff5f2ebd2ea17117166c03c3dfa3f8a17c603cf
cabelectronics <cabelectronicsinfo@gmail.com>
2022-05-26 13:32:15 +0200

    hello

---
+++ bego.py
@@ -0,0 +1,21 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
+from PyQt5.QtGui import QIcon
+
+class App(QWidget):
+
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
+        if fileName:
+            print(fileName)
+        self.show()
+
+    
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    ex.show()
+    
+    sys.exit(app.exec_())
