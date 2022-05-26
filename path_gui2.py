00f3b09552d998be5071e44361808ca8d4919bb5
Begoña Calderón <begona.calderon@student.bisstgeorges.com>
2022-05-26 13:29:26 +0200

    jjj

---
+++ path_gui2.py
@@ -0,0 +1,42 @@
+import os
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QComboBox, QPushButton, QFileDialog, QVBoxLayout
+
+class MyApp(QFileDialog):
+
+    def getDirectory(self):
+        response = QFileDialog.getExistingDirectory(
+            self,
+            caption='Select a folder',
+            directory=os.getcwd())
+        dialog=QFileDialog()
+        dialog.setFileMode(QFileDialog.Directory)
+        dialog.setOption(QFileDialog.DontUseNativeDialog, True)
+       
+        print(response)
+        return response 
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyleSheet('''
+        QWidget {
+            font-size: 35px;
+        }
+    ''')
+    
+    myApp = MyApp()
+    myApp.show()
+
+    try:
+        sys.exit(app.exec_())
+    except SystemExit:
+        print('Closing Window...')
+
+    
+
+
+
+    
+
+
